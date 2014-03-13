086cdcf4a3e486911ddee8568189a8f79ccb17f2
andrealveslima <andrealveslimeira@gmail.com>
2014-03-13 16:23:14 +0100

    Versão final do exemplo

---
+++ ProjetoDeUnitTests/ClasseASerTestadaTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjetoDeUnitTests
+{
+    [TestClass]
+    public class ClasseASerTestadaTests
+    {
+        [TestMethod]
+        public void ClasseASerTestadaTest()
+        {
+            LibraryASerTestada.ClasseASerTestada instancia = new LibraryASerTestada.ClasseASerTestada();
+
+            Assert.IsTrue(instancia.MetodoPublicoQueRetornaTrue());
+            Assert.IsFalse(instancia.MetodoInternalQueRetornaFalse());
+        }
+    }
+}
