284e69ca679a2d5991296f6dd16b5ea241c70cda
andrealveslima <andrealveslimeira@gmail.com>
2014-03-13 16:23:33 +0100

    Arquivos faltando

---
+++ LibraryASerTestada/ClasseASerTestada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryASerTestada
+{
+    public class ClasseASerTestada
+    {
+        public bool MetodoPublicoQueRetornaTrue()
+        {
+            return true;
+        }
+        internal bool MetodoInternalQueRetornaFalse()
+        {
+            return false;
+        }
+    }
+}
